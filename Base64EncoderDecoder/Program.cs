997ad5f05c5398d266f5197d60933bb01a0a1176
Felipe Sanches <felipe_sanches94@hotmail.com>
2023-08-04 16:37:40 -0300

    Add project files.

---
+++ Base64EncoderDecoder/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+Console.WriteLine("Aperte C para codificar um arquivo, D para decodificar:");
+ConsoleKeyInfo key = Console.ReadKey();
+Console.WriteLine();
+
+if (key.KeyChar.ToString().ToUpper() == "C")
+{
+
+
+    Console.WriteLine("Informe o local do arquivo:");
+    string file = Console.ReadLine();
+    try
+    {
+        StreamReader reader = new StreamReader(file);
+        string content = reader.ReadToEnd();
+        reader.Close();
+
+
+        byte[] fileBytes = File.ReadAllBytes(file);
+        string base64EncodedString = Convert.ToBase64String(fileBytes);
+
+        Console.WriteLine("Arquivo codificado em Base64:");
+        Console.WriteLine(base64EncodedString);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine($"Error {error.Message}");
+    }
+}
+
+else if (key.KeyChar.ToString().ToUpper() == "D")
+{
+    Console.WriteLine("Informe o valor codificado:");
+    string encodedText = Console.ReadLine();
+    try
+    {
+        byte[] decodedBytes = Convert.FromBase64String(encodedText);
+        string decodedString = System.Text.Encoding.UTF8.GetString(decodedBytes);
+
+        Console.WriteLine("Conteúdo decodificado:");
+        Console.WriteLine(decodedString);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine($"Error {error.Message}");
+    }
+
+
+}
+else
+{
+    Console.WriteLine("Tecla invalida finalizando ....");
+}
+
---
+++ Read2Create1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Passe o local do primeiro arquivo:");
+string? filePath1 = Console.ReadLine();
+Console.WriteLine("Passe o local do segundo arquivo:");
+string? filePath2 = Console.ReadLine();
+Console.WriteLine("Passe o local que deseja salvar o arquivo");
+string? pathSave = Console.ReadLine();
+
+try
+{
+    StreamReader reader1 = new StreamReader(filePath1);
+    string content1 = reader1.ReadToEnd();
+    reader1.Close();
+
+    StreamReader reader2 = new StreamReader(filePath2);
+    string content2 = reader2.ReadToEnd();
+    reader2.Close();
+
+    string contentNew = $"{content1}\n\n{content2}";
+
+    StreamWriter sw = new StreamWriter($"{pathSave}\\terceiroArquivo.txt");
+
+    sw.WriteLine(contentNew);
+    sw.Close();
+    Console.WriteLine($"Terceiro arquivo criado com sucesso, local {pathSave}\\terceiroArquivo.txt");
+
+
+}
+catch (Exception error)
+{
+    Console.WriteLine($"Error {error.Message}");
+}
+
+
